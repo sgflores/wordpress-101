57ccb72c706994479f693127b20611dd37a22b77
opic <floresopic@gmail.com>
2020-12-28 21:16:01 +0800

    Initial orders html table

--- wp-content/themes/twentytwentyone-child/orders/index.php
+++ wp-content/themes/twentytwentyone-child/orders/index.php
@@ -1,3 +1,36 @@
 <?php 
 /* Template Name: CustomWPPage */
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Custom WP Page</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+</head>
+<body>
+        
+    <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <p class="h5 my-0 me-md-auto fw-normal">The Company</p>
+    </header>
+
+    <main class="container"  id="app">
+        <table class="table table-sm table-striped table-hover">
+            <thead>
+                <th>Order ID</th>
+                <th>Date Submitted</th>
+                <th>Client Email</th>
+                <th>Article ID</th>
+                <th>Required Word Count</th>
+            </thead>
+        </table>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+</body>
+</html>
